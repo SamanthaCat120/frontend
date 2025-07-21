0ca5348a902eba87ce1564c7561561cd878f2b75
SamanthaCat120 <53274929+SamanthaCat120@users.noreply.github.com>
2025-07-20 20:09:12 -0400

    add details pages

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,8 @@ addEventListener("DOMContentLoaded", async function(){
 
     let html = ""
     for (let song of songs) {
-        html += `<li>${song.title} - ${song.artist}</li>`
+        let songID = song._id
+        html+=`<li>${song.title} - ${song.artist} - <a href="details.html?id=${songID}">Details</a> - <a href="edit.html?id=${songID}">Edit Song</a> </li></li>`
     }
     document.querySelector("#list_of_songs").innerHTML = html
 
